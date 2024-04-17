0f8dd6a86967088d4b076699f4deb7daf3f719e7
ksdeveloper0201 <ksdeveloper0201@gmail.com>
2024-04-18 01:49:30 +0900

    引数を渡すチュートリアルその2

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 // In App.js in a new project
 
 import * as React from "react";
-import { Button, View, Text } from "react-native";
+import { Button, View, Text, TextInput } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 type NavigationProps = {
     navigation: any;
+    route: any;
 };
 
 type DetailNavigationProps = {
@@ -14,23 +15,55 @@ type DetailNavigationProps = {
     navigation: any;
 };
 
-function HomeScreen({ navigation }: NavigationProps) {
+function HomeScreen({ navigation, route }: NavigationProps) {
+    React.useEffect(() => {
+        if (route.params?.post) {
+            // Post updated, do something with `route.params.post`
+            // For example, send the post to the server
+        }
+    }, [route.params?.post]);
+
     return (
         <View
             style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
         >
-            <Text>Home Screen</Text>
             <Button
-                title="Go to Details"
+                title="Create Post"
+                onPress={() => navigation.navigate("CreatePost")}
+            />
+            <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
+        </View>
+    );
+}
+
+type CreatePostScreenProps = {
+    navigation: any;
+    route: any;
+};
+
+function CreatePostScreen({ navigation, route }: CreatePostScreenProps) {
+    const [postText, setPostText] = React.useState("");
+
+    return (
+        <>
+            <TextInput
+                multiline
+                placeholder="Whats on your mind?"
+                style={{ height: 200, padding: 10, backgroundColor: "white" }}
+                value={postText}
+                onChangeText={setPostText}
+            />
+            <Button
+                title="Done"
                 onPress={() => {
-                    /* 1. Navigate to the Details route with params */
-                    navigation.navigate("Details", {
-                        itemId: 86,
-                        otherParam: "anything you want here",
+                    navigation.navigate({
+                        name: "Home",
+                        params: { post: postText },
+                        merge: true,
                     });
                 }}
             />
-        </View>
+        </>
     );
 }
 
@@ -72,6 +105,7 @@ function App() {
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Home">
                 <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="CreatePost" component={CreatePostScreen} />
                 <Stack.Screen
                     name="Details"
                     component={DetailsScreen}
