03f501f64d33cb78d0b7055ee41cf35c368fee43
ksdeveloper0201 <ksdeveloper0201@gmail.com>
2024-04-18 01:16:14 +0900

    引数を渡すチュートリアルを実施

--- App.tsx
+++ App.tsx
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as React from "react";
+import { Button, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+type NavigationProps = {
+    navigation: any;
+};
+
+type DetailNavigationProps = {
+    route: any;
+    navigation: any;
+};
+
+function HomeScreen({ navigation }: NavigationProps) {
+    return (
+        <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+            <Text>Home Screen</Text>
+            <Button
+                title="Go to Details"
+                onPress={() => {
+                    /* 1. Navigate to the Details route with params */
+                    navigation.navigate("Details", {
+                        itemId: 86,
+                        otherParam: "anything you want here",
+                    });
+                }}
+            />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen({ route, navigation }: DetailNavigationProps) {
+    const { itemId, otherParam } = route.params;
+    return (
+        <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+            <Text>Details Screen</Text>
+            <Text>itemId: {JSON.stringify(itemId)}</Text>
+            <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+            <Button
+                title="Go to Details... again"
+                onPress={() =>
+                    navigation.push("Details", {
+                        itemId: Math.floor(Math.random() * 100),
+                        otherParam: "more secondary",
+                    })
+                }
+            />
+            <Button
+                title="Go to Home"
+                onPress={() => navigation.navigate("Home")}
+            />
+            <Button title="Go back" onPress={() => navigation.goBack()} />
+            <Button
+                title="Go back to first screen in stack"
+                onPress={() => navigation.popToTop()}
+            />
+        </View>
+    );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen
+                    name="Details"
+                    component={DetailsScreen}
+                    initialParams={{ itemId: 42 }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default App;
